01a7755ee646918e4894a82d93a721e35dcbb164
seyeonn <ansse17@gmail.com>
2023-02-23 18:09:07 +0900

    kotlin step4

---
+++ src/main/kotlin/step4/BJ10813.kt
@@ -0,0 +1,24 @@
+package step4
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    val M = nextInt()
+    val arr = Array<Int>(N) {i -> i+1 }
+
+    for (t in 0 until M) {
+        var i = nextInt() -1
+        var j = nextInt() -1
+
+        if(i < j) {
+            val temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
+    }
+
+    for (i in 0..arr.size-1){
+        print("${arr[i]} ")
+    }
+}
